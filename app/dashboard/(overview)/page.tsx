7de642333520e63f2e3e755ae3ed2683e66ad47d
Kargobober <kargobober@yandex.ru>
2024-03-19 13:14:27 +0300

    finish chapter 9

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,35 @@
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import CardWrapper from '@/app/ui/dashboard/cards';
+import {
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+  CardsSkeleton
+} from '@/app/ui/skeletons';
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        {/* добавил троттлинг в 5 секунд для CardWrapper (внутри запроса данных) */}
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          {/* данный компонент мы не троттлим вручную, потому его скелетон не видно, хоть мы его и задали */}
+          <LatestInvoices />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
